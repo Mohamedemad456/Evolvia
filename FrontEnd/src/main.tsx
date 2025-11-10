7ea54fbbd496af525538ac5a4a40e315362911ea
Mohamedemad456 <mohamed.asker316@gmail.com>
2025-11-10 22:47:34 +0200

    fix github pages

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -4,9 +4,24 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
+// Single Page Apps for GitHub Pages
+// https://github.com/rafgraph/spa-github-pages
+// This script checks if we're on a 404 page and redirects to index.html
+// with the path as a query parameter, then React Router will handle it
+(function(l) {
+  if (l.search[1] === '/' ) {
+    var decoded = l.search.slice(1).split('&').map(function(s) { 
+      return s.replace(/~and~/g, '&')
+    }).join('?');
+    window.history.replaceState(null, '',
+        l.pathname.slice(0, -1) + decoded + l.hash
+    );
+  }
+}(window.location))
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Evolvia">
       <App />
     </BrowserRouter>
   </StrictMode>,
